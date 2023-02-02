f237bb7a3e80e7b9eda0fa781679490f845d6ed0
TaylorAishman <AishmanTD@hendrix.edu>
2023-02-01 20:52:11 -0600

    Implemented UltrasonicAvoid

--- ForwardBump.py
+++ ForwardBump.py
@@ -5,20 +5,20 @@ ev3 = EV3Brick()
 ev3.screen.clear()
 ev3.screen.draw_text(0, 0, "ForwardBumpFunction")
 
-left = Motor(Port.A)
-right = Motor(Port.D)
+left_motor = Motor(Port.A)
+right_motor = Motor(Port.D)
+left_bumper = TouchSensor(Port.S1)
+right_bumper = TouchSensor(Port.S4)
 
 SPEED = 360
-REFRESH = 10000
-
-left.run(SPEED)
-right.run(SPEED)
+left_motor.run(SPEED)
+right_motor.run(SPEED)
 
 while True: 
     pressed = ev3.buttons.pressed()
-    if pressed:
-        left.run(0)
-        right.run(0)
+    if left_bumper.pressed or right_bumper.pressed:
+        left_motor.run(0)
+        right_motor.run(0)
     else: 
-        left.run(SPEED)
-        right.run(SPEED)
+        left_motor.run(SPEED)
+        right_motor.run(SPEED)
--- UltrasonicAvoid.py
+++ UltrasonicAvoid.py
@@ -1,28 +1,25 @@
 import time
 from pybricks.hubs import EV3Brick
 from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
 
 ev3 = EV3Brick()
 ev3.screen.clear()
-ev3.screen.draw_text(0, 0, "BumpAvoid")
+ev3.screen.draw_text(0, 0, "UltrasonicAvoid")
 
 left_motor = Motor(Port.A)
 right_motor = Motor(Port.D)
-left_bumper = TouchSensor(Port.S1)
-right_bumper = TouchSensor(Port.S4)
-
+ev3sonar = UltrasonicSensor(Port.S4)
 
 SPEED = 360
+REFRESH = 10000
+
 left_motor.run(SPEED)
 right_motor.run(SPEED)
 
-while True: 
-    pressed = ev3.buttons.pressed()
-    if left_motor.pressed:
-        left_bumper.run(0)
-        right_bumper.run(SPEED)
-        time.wait(5)
-    if right_motor.pressed:
+while True:
+    if ev3sonar.distance < 10:
         left_bumper.run(SPEED)
         right_bumper.run(0)
         time.wait(5)
