78bb67459c6cdf382e349bd03b43f2b4f71d2b69
TaylorAishman <AishmanTD@hendrix.edu>
2023-02-01 23:19:52 -0600

    Final Changes

--- CombinedAvoid.py
+++ CombinedAvoid.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env pybricks-micropython
+#!/usr/bin/env python3
 from pybricks.hubs import EV3Brick
 from pybricks.parameters import Port
 from pybricks.ev3devices import (Motor, TouchSensor, UltrasonicSensor)
@@ -18,20 +19,23 @@ left_motor.run(SPEED)
 right_motor.run(SPEED)
 
 while True:    
-    if (not ev3sonar.distance() < 75 or not left_bumper.pressed() or not right_bumper.pressed()):
+    if (not ev3sonar.distance < 75 or not left_bumper.pressed() or not right_bumper.pressed()):
         left_motor.run(SPEED)
         right_motor.run(SPEED)  
-    elif (ev3sonar.distance() < 75 or left_bumper.pressed()):
+    elif ev3sonar.distance < 75 and left_bumper.pressed():
         for x in range(0,1000):
             left_motor.run(-SPEED)
             right_motor.run(-SPEED)
         for x in range(0,1000):
             left_motor.run(-SPEED)
             right_motor.run(SPEED)
-    elif (ev3sonar.distance() < 75 or right_bumper.pressed()):
+    elif ev3sonar.distance() < 75 and right_bumper.pressed():
         for x in range(0,1000):
             left_motor.run(-SPEED)
             right_motor.run(-SPEED)
         for x in range(0,1000):
             left_motor.run(-SPEED)
             right_motor.run(SPEED)
+    else: 
+        left_motor.run(SPEED)
+        right_motor.run(SPEED)  
